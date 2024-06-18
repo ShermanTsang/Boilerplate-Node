d95e0952cfbcacb1a54d68ff3d7491de2210e962
ShermanT <sharemant@163.com>
2024-06-18 12:11:57 +0800

    chore: init project (sync local code to git version control)

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// import Pkg from 'pkg';
+//
+// console.log(Pkg);
+
+const test: Example.SingleItem = {
+    name: '1'
+};
+
+setTimeout(() => {
+    console.log(5);
+}, 50000);
+
+console.log(test);
+console.log(process.env.NODE_ENV);
